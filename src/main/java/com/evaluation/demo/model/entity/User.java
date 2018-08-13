1050ba80783ef59cdab8d5fe8bc83dbfeead5ee4
emil <emil.m@o2.pl>
2018-08-13 09:10:15 +0200

    em entity

---
+++ src/main/java/com/evaluation/demo/model/entity/User.java
@@ -0,0 +1,19 @@
+package com.evaluation.demo.model.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private String github;
+    private String phone;
+
+}
