c292de9bc38ca81341c72cddbcbc53adafbc4476
emil <emil.m@o2.pl>
2018-08-13 11:16:58 +0200

    register page

--- src/main/java/com/evaluation/demo/controller/MainController.java
+++ src/main/java/com/evaluation/demo/controller/MainController.java
@@ -3,9 +3,15 @@ package com.evaluation.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
+
 public class MainController {
     @GetMapping("/login")
     public String login(){
         return "/login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "/register";
     }
 }
