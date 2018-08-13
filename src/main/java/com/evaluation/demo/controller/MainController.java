b6f640296f5fce21f622bd1382b0d4bf0885bf4b
charliefreak <adriana.dzienis@gmail.com>
2018-08-13 12:38:18 +0200

    homepage, contact page

--- src/main/java/com/evaluation/demo/controller/MainController.java
+++ src/main/java/com/evaluation/demo/controller/MainController.java
@@ -3,15 +3,14 @@ package com.evaluation.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
-
 public class MainController {
     @GetMapping("/")
     public String login(){
-        return "/homePage";
+        return "homepage";
+    }
+    @GetMapping("/contact")
+    public String kontakt() {
+        return "contact";
     }
 
-    @GetMapping("/register")
-    public String register() {
-        return "/register";
-    }
 }
